5d37e5f5c755601218f030ee15997e6e67ec1e0e
will <402876740@qq.com>
2015-08-10 21:02:20 +0800

    commit

--- pickerView/ViewController.m
+++ pickerView/ViewController.m
@@ -8,20 +8,62 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIPickerViewDataSource,UIPickerViewDelegate>{
+    NSUInteger _selectedRow;
+}
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIPickerView *pick = [[UIPickerView alloc] initWithFrame:CGRectMake(100, 300, 200, 100)];
+    pick.delegate = self;
+    [self.view addSubview:pick];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    _selectedRow = row;
+    [pickerView reloadComponent:component];
 }
+
+
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
+
+{
+    BOOL isSelectedRow = row == _selectedRow;
+    
+    NSString *yourString = [self pickerView:pickerView titleForRow:row forComponent:component];
+    NSDictionary *attributeDict = @{
+                                    NSForegroundColorAttributeName : isSelectedRow?[UIColor blueColor]:[UIColor blackColor],
+                                    NSFontAttributeName :isSelectedRow?[UIFont systemFontOfSize:18]:[UIFont systemFontOfSize:15]
+                                    };
+    NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:yourString attributes:attributeDict];
+    
+    UILabel *label = [[UILabel alloc] init];
+    label.textAlignment = NSTextAlignmentLeft;
+    label.backgroundColor = [UIColor clearColor];
+    label.attributedText = attributedString;
+    
+    return label;
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return @"123";
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    
+    return 111;
+}
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return  2;
+}
+
 
 @end
