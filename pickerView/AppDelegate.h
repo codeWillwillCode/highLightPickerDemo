dc64190087dd7be64300bd4022fb582769dcc6c3
will <402876740@qq.com>
2015-08-10 20:48:59 +0800

    Initial Commit

---
+++ pickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pickerView
+//
+//  Created by 叶 on 15/8/10.
+//  Copyright (c) 2015年 six. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pickerView
+//
+//  Created by 叶 on 15/8/10.
+//  Copyright (c) 2015年 six. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
